bc2be9cb7e65c8a8e17a574b6f68d87f2714b255
Peres_05 <isaacperes05@gmail.com>
2024-10-30 16:48:39 -0300

    2024.10.30

---
+++ .database/database.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS cadtrecos;
+CREATE DATABASE cadtrecos
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE cadtrecos;
+
+CREATE TABLE owner (
+    o_id INT PRIMARY KEY AUTO_INCREMENT,
+    o_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    o_birth DATE,
+    o_name VARCHAR(127) NOT NULL,
+    o_email VARCHAR(255) NOT NULL,
+    o_pass VARCHAR(63) NOT NULL,
+    o_status ENUM('on', 'off', 'del') DEFAULT 'on'
+);
+
+CREATE TABLE thing (
+    t_id INT PRIMARY KEY AUTO_INCREMENT,
+    t_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    t_owner INT NOT NULL,
+    t_photo VARCHAR(255),
+    t_name VARCHAR(127) NOT NULL,
+    t_description TEXT,
+    t_location VARCHAR(255),
+    t_status ENUM('on', 'off', 'del') DEFAULT 'on',
+    FOREIGN KEY (t_owner) REFERENCES owner (o_id)
+);
+
+
+INSERT INTO owner (
+    o_birth,
+    o_name,
+    o_email,
+    o_pass
+) VALUES (
+    '2000-10-28',
+    'Joca da Silva',
+    'jocasilva@email.com',
+    SHA1('Senha123')
+), (
+    '1999-10-11',
+    'Marineuza Siriliano',
+    'marineuza@email.com',
+    SHA1('Senha123')
+);
+
+INSERT INTO thing (t_owner, t_photo, t_name, t_description, t_location) VALUES
+(1, 'https://picsum.photos/196', 'peteca', 'Cheia de penas velhas', 'Caixa de sapato sob a cama'),
+(2, 'https://picsum.photos/197', 'caneca', 'Caneca de cerâmica verde', 'Prateleira da cozinha'),
+(1, 'https://picsum.photos/198', 'livro', 'Livro de capa dura', 'Estante do escritório'),
+(2, 'https://picsum.photos/199', 'relógio', 'Relógio de parede antigo', 'Sala de estar'),
+(1, 'https://picsum.photos/200', 'ferramenta', 'Martelo de carpinteiro', 'Caixa de ferramentas na garagem'),
+(2, 'https://picsum.photos/201', 'vaso', 'Vaso de vidro transparente', 'Mesa da sala de jantar'),
+(1, 'https://picsum.photos/202', 'brinquedo', 'Carrinho de brinquedo', 'Quarto das crianças'),
+(2, 'https://picsum.photos/203', 'guitarra', 'Guitarra elétrica vermelha', 'Estúdio de música'),
+(1, 'https://picsum.photos/204', 'pintura', 'Quadro abstrato colorido', 'Parede do corredor'),
+(2, 'https://picsum.photos/205', 'cadeira', 'Cadeira de escritório ergonômica', 'Home office');
---
+++ app.py
@@ -0,0 +1,249 @@
+from datetime import datetime, timedelta
+import json
+from flask import Flask, make_response, redirect, render_template, request, url_for, g
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configurações de acesso ao MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'cadtrecos'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_USE_UNICODE'] = True
+app.config['MYSQL_CHARSET'] = 'utf8mb4'
+
+mysql = MySQL(app)
+
+
+@app.before_request
+def before_request():
+    cur = mysql.connection.cursor()
+    cur.execute("SET NAMES utf8mb4")
+    cur.execute("SET character_set_connection=utf8mb4")
+    cur.execute("SET character_set_client=utf8mb4")
+    cur.execute("SET character_set_results=utf8mb4")
+    cur.execute("SET lc_time_names = 'pt_BR'")
+    cur.close()
+
+
+@app.route('/')
+def home():
+
+    edit = request.args.get('edit')
+    delete = request.args.get('delete')
+
+    cookie = request.cookies.get('user_data')
+
+    if cookie == None:
+        return redirect(f"{url_for('login')}")
+
+    user = json.loads(cookie)
+    user['fname'] = user['name'].split()[0]
+
+    sql = '''
+        SELECT t_id, t_photo, t_name, t_status
+        FROM thing
+        WHERE
+            t_owner = %s
+            AND t_status != 'del'
+        ORDER BY t_date DESC;
+    '''
+    cur = mysql.connection.cursor()
+    cur.execute(sql, (user['id'], ))
+    things = cur.fetchall()
+    cur.close()
+
+    return render_template('home.html', things=things, user=user, edit=edit, delete=delete)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    error = ''
+
+    if request.method == 'POST':
+
+        form = dict(request.form)
+
+        print('\n\n\n', form, '\n\n\n')
+
+        sql = '''
+            SELECT o_id, o_name
+            FROM owner
+            WHERE o_email = %s
+                AND o_pass = SHA1(%s)
+                AND o_status = 'on';
+        '''
+        cur = mysql.connection.cursor()
+        cur.execute(sql, (form['email'], form['password'], ))
+        user = cur.fetchone()
+        cur.close()
+
+        # print('\n\n\n', user, '\n\n\n')
+
+        if user != None:
+
+            resp = make_response(redirect(url_for('home')))
+
+            cookie_data = {
+                'id': user['o_id'],
+                'name': user['o_name']
+            }
+
+            print('\n\n\nCookie:', cookie_data, '\n\n\n')
+            # Data em que o cookie expira
+            expires = datetime.now() + timedelta(days=365)
+            # Adicona o cookie à página
+            resp.set_cookie('user_data', json.dumps(
+                cookie_data), expires=expires)
+
+            return resp
+
+        else:
+            error = 'Login e/ou senha errados!'
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+
+    success = False
+
+    cookie = request.cookies.get('user_data')
+
+    if cookie == None:
+        return redirect(f"{url_for('login')}")
+
+    user = json.loads(cookie)
+    user['fname'] = user['name'].split()[0]
+
+    if request.method == 'POST':
+        form = dict(request.form)
+
+        sql = '''
+            INSERT INTO thing (
+                t_owner, t_photo, t_name, t_description, t_location
+            ) VALUES (
+                %s, %s, %s, %s, %s
+            )
+        '''
+        cur = mysql.connection.cursor()
+        cur.execute(sql, (user['id'], form['photo'],
+                    form['name'], form['description'], form['location']))
+        mysql.connection.commit()
+        cur.close()
+
+        success = True
+
+    return render_template('new.html', user=user, success=success)
+
+
+@app.route('/view/<id>')
+def view(id):
+
+    cookie = request.cookies.get('user_data')
+
+    if cookie == None:
+        return redirect(f"{url_for('login')}")
+
+    user = json.loads(cookie)
+    user['fname'] = user['name'].split()[0]
+
+    sql = '''
+        SELECT t_id, t_date, t_photo, t_name, t_description, t_location
+        FROM thing
+        WHERE t_status = 'on' AND t_owner = %s AND t_id = %s
+    '''
+    cur = mysql.connection.cursor()
+    cur.execute(sql, (user['id'], id,))
+    thing = cur.fetchone()
+    cur.close()
+
+    print('\n\n\n', thing, '\n\n\n')
+
+    return render_template('view.html', user=user, thing=thing)
+
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+
+    cookie = request.cookies.get('user_data')
+
+    if cookie == None:
+        return redirect(url_for('login'))
+
+    user = json.loads(cookie)
+    user['fname'] = user['name'].split()[0]
+
+    if request.method == 'POST':
+
+        form = dict(request.form)
+
+        sql = '''
+            UPDATE thing SET 
+                t_photo = %s,
+                t_name = %s,
+                t_description = %s,
+                t_location = %s
+            WHERE t_status = 'on'
+                AND t_owner = %s
+                AND t_id = %s
+        '''
+        cur = mysql.connection.cursor()
+        cur.execute(sql, (form['photo'], form['name'],
+                    form['description'], form['location'], user['id'], id,))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('home', edit=True))
+
+    sql = '''
+        SELECT t_id, t_date, t_photo, t_name, t_description, t_location
+        FROM thing
+        WHERE t_status = 'on' AND t_owner = %s AND t_id = %s
+    '''
+    cur = mysql.connection.cursor()
+    cur.execute(sql, (user['id'], id,))
+    thing = cur.fetchone()
+    cur.close()
+
+    return render_template('edit.html', user=user, thing=thing)
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    cookie = request.cookies.get('user_data')
+    if cookie == None:
+        return redirect(url_for('login'))
+    user = json.loads(cookie)
+    user['fname'] = user['name'].split()[0]
+
+    sql = '''
+        UPDATE thing SET
+            t_status = 'del'
+        WHERE t_owner = %s
+            AND t_id = %s
+    '''
+    cur = mysql.connection.cursor()
+    cur.execute(sql, (user['id'], id,))
+    mysql.connection.commit()
+    cur.close()
+
+    return redirect(url_for('home', delete=True))
+
+
+@app.route('/logout')
+def logout():
+
+    resp = make_response(redirect(url_for('login')))
+
+    resp.set_cookie('user_data', '', expires=0)
+
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
